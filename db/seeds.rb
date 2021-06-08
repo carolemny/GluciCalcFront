47d099a5e286d479c48d3eb06090ea8665fd5921
Morgane Tessier <77018536+m-tessier@users.noreply.github.com>
2021-06-08 09:36:43 +0200

    First deployment (#8)
    * Update cors file
    
    * Add devise and jwt
    
    * Gemfile.lock
    
    * Add property table
    
    * Add seed
    
    * Add restrictions for properties
    
    * Change rails api localhost port
    
    * Add validations for properties
    
    * Implement mailer for development
    
    * Implement mailer for production
    
    * Update user controller
    
    Co-authored-by: martinfzz <forget.l.martin@gmail.com>
    Co-authored-by: Martinfzz <71545892+Martinfzz@users.noreply.github.com>
    Co-authored-by: carolemny <meneycarole@gmail.com>
    Co-authored-by: carolemny <76789941+carolemny@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Property.destroy_all
+
+10.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Name.first_name + "@yopmail.com",
+    password: "azerty",
+  )
+  puts "Create User"
+end
+
+30.times do 
+  property = Property.create!(
+    title: Faker::Book.title,
+    description: Faker::Lorem.sentence,
+    price: Faker::Number.number(digits: 6),
+    user_id: User.all.sample.id,
+  )  
+  puts "Create Proprety"
+end
