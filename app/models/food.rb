98c9998c2002e576f9d7d591b982210bac42090c
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-15 11:57:55 +0200

    update foreign key association

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,10 +1,12 @@
 class Food < ApplicationRecord
-  validates :type, presence: true
+  validates :category, presence: true
   validates :name, presence: true, length: {minimum: 1, maximum: 50}
   validates :description, length: {minimum: 1, maximum: 1000}
-  has_many :quantities
+  
+  has_many :quantities, foreign_key: :recipe_id
+  has_many :quantities, foreign_key: :product_id
   has_many :product_id, class_name: "Quantity", foreign_key: "product_id"
   has_many :recipe_id, class_name: "Quantity", foreign_key: "recipe_id"
-  has_many :comments
-  has_many :favorites
+  has_many :comments, foreign_key: :recipe_id
+  has_many :favorites, foreign_key: :recipe_id
 end
