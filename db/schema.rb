38d2f5f26303e184d26731625deb71d9cb2d6a10
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-15 10:33:45 +0200

    favorite table scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_075323) do
+ActiveRecord::Schema.define(version: 2021_06_15_082336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_favorites_on_recipe_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "api_product_id"
