ed0d23acc9e48b44400aa8484c9b197ae5591dc3
Morgane Tessier <77018536+m-tessier@users.noreply.github.com>
2021-06-17 17:04:00 +0200

    update url for mailer (#13)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url  = 'https://react-immo-coin.vercel.app/login'
+    @url  = 'https://react-glucicalc.vercel.app/login'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 
   def forgot_password_email(user)
     @user = user
-    @url  = 'https://react-immo-coin.vercel.app/password/reset'
+    @url  = 'https://react-glucicalc.vercel.app/password/reset'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 end
