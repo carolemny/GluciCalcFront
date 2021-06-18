05bd9df5ab7ac0652f7a399070870f8c98ac9f4c
JAG-ROSA <76993925+JAG-ROSA@users.noreply.github.com>
2021-06-18 10:16:44 +0200

    add dependent destroy to quantities (#17)

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,7 @@
 class Meal < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
-  has_many :quantities
+  has_many :quantities, dependent: :destroy
 
   def as_json(options)
     super(include: { quantities: { include: [:product, :recipe] } })
