af311aa70a2de0373e6e8568bbc96d287b1af276
martinfzz <forget.l.martin@gmail.com>
2021-06-25 12:20:02 +0200

    Update glucicalc url mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url  = 'https://react-glucicalc.vercel.app/login'
+    @url  = 'https://gluci-calc.vercel.app/login'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 
   def forgot_password_email(user)
     @user = user
-    @url  = 'https://react-glucicalc.vercel.app/password/reset'
+    @url  = 'https://gluci-calc.vercel.app/password/reset'
     mail(to: @user.email, subject: 'Changement de mot de passe')
   end
 end
